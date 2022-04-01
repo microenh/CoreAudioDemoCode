f85ef882b2798df8fe9f35f75b92804727b3ce66
Mark Erbaugh <mark.election@gmail.com>
2022-03-31 20:47:54 -0400

    Issues with srcNode

--- CoreAudioDemoCode/CAAudioEnginePlayThroughMEE.swift
+++ CoreAudioDemoCode/CAAudioEnginePlayThroughMEE.swift
@@ -7,6 +7,12 @@
 
 import AVFoundation
 
+#if PART_II
+struct Settings {
+    static let speakString = "The engine is running" as CFString
+}
+#endif
+
 // MARK: user-data struct
 struct MyAUEnginePlayer {
     var streamFormat = AudioStreamBasicDescription()
@@ -16,6 +22,7 @@ struct MyAUEnginePlayer {
     var outputUnit: AudioUnit!
     
 #if PART_II
+    var speechUnit: AudioUnit!
 #endif
     
     var inputBuffer: UnsafeMutableAudioBufferListPointer!
@@ -86,8 +93,6 @@ func graphRenderProc(inRefCon: UnsafeMutableRawPointer,
     return outputProcErr
 }
 
-// typealias AVAudioSourceNodeRenderBlock = (UnsafeMutablePointer<ObjCBool>, UnsafePointer<AudioTimeStamp>, AVAudioFrameCount, UnsafeMutablePointer<AudioBufferList>) -> OSStatus
-
 // MARK: utility functions
 func createInputUnit(player: UnsafeMutablePointer<MyAUEnginePlayer>) throws {
     // Ger input unit from HAL
@@ -109,8 +114,8 @@ func createInputUnit(player: UnsafeMutablePointer<MyAUEnginePlayer>) throws {
     try player.pointee.streamFormat = player.pointee.inputUnit.getABSD(scope: kAudioUnitScope_Output, element: 1)
     let deviceFormat = try player.pointee.inputUnit.getABSD(scope: kAudioUnitScope_Input, element: 1)
     
-    print ("in:  \(deviceFormat)")
-    print ("out: \(player.pointee.streamFormat)")
+    // print ("in:  \(deviceFormat)")
+    // print ("out: \(player.pointee.streamFormat)")
     
     player.pointee.streamFormat.mSampleRate = deviceFormat.mSampleRate
     
@@ -119,7 +124,7 @@ func createInputUnit(player: UnsafeMutablePointer<MyAUEnginePlayer>) throws {
     let bufferSizeFrames = try player.pointee.inputUnit.getBufferFrameSize()
     let bufferSizeBytes = bufferSizeFrames * UInt32(MemoryLayout<Float32>.size)
     
-    print ("format is \(player.pointee.streamFormat.mFormatFlags & kAudioFormatFlagIsNonInterleaved > 0 ? "non-" : "")interleaved")
+    // print ("format is \(player.pointee.streamFormat.mFormatFlags & kAudioFormatFlagIsNonInterleaved > 0 ? "non-" : "")interleaved")
     
     let channelCount = Int(player.pointee.streamFormat.mChannelsPerFrame)
     
@@ -154,21 +159,62 @@ func createMyAVEngine(player: UnsafeMutablePointer<MyAUEnginePlayer>) throws {
     let outputNode = player.pointee.engine.outputNode
     player.pointee.outputUnit = outputNode.audioUnit
     
+    let outputFormat = outputNode.inputFormat(forBus: 0)
+    
+    let inputFormat = AVAudioFormat(commonFormat: outputFormat.commonFormat,
+                                    sampleRate: outputFormat.sampleRate,
+                                    channels: 2,
+                                    interleaved: outputFormat.isInterleaved)
+    
+    let srcNode = AVAudioSourceNode { isSilence, inTimeStamp, frameCount, ioData -> OSStatus in
+        if player.pointee.firstOutputSampleTime < 0 {
+            player.pointee.firstOutputSampleTime = inTimeStamp.pointee.mSampleTime
+            if player.pointee.firstInputSampleTime > 0 && player.pointee.inToOutSampleTimeOffset < 0 {
+                player.pointee.inToOutSampleTimeOffset = player.pointee.firstInputSampleTime - player.pointee.firstOutputSampleTime
+            }
+        }
+        
+        // copy samples out of ring buffer
+        let outputProcErr = FetchBuffer(player.pointee.ringBuffer,
+                                        ioData,
+                                        frameCount,
+                                        Int64(inTimeStamp.pointee.mSampleTime + player.pointee.inToOutSampleTimeOffset))
+        // isSilence.pointee = ObjCBool(outputProcErr != 0)
+        if outputProcErr > 0 {
+            print (outputProcErr)
+        }
+        return noErr
+    }
+    
+    
+    player.pointee.engine.attach(srcNode)
+    
+    // get the engine's mixer node
+    let mixerNode = player.pointee.engine.mainMixerNode
 #if PART_II
+        
+    // add the speech synthesizer to the graph
+    let cd = AudioComponentDescription(componentType: kAudioUnitType_Generator,
+                                       componentSubType: kAudioUnitSubType_SpeechSynthesis)
+    let speechNode = AVAudioUnitGenerator(audioComponentDescription: cd)
+    player.pointee.engine.attach(speechNode)
+    player.pointee.speechUnit = speechNode.audioUnit
+    
+    player.pointee.engine.connect(speechNode, to: mixerNode, format: inputFormat)
+
 #endif
-    
-    
-    // Set the stream format on the output unit's input scope
-    try player.pointee.outputUnit.setABSD(absd: player.pointee.streamFormat, scope: kAudioUnitScope_Input)
-
-    try player.pointee.outputUnit.setRenderCallback(inputProc: graphRenderProc, inputProcRefCon: player)
-
-    player.pointee.engine.connect(player.pointee.engine.inputNode,
-                                  to: outputNode, format: nil)
-
-    
+    player.pointee.engine.connect(srcNode, to: mixerNode, format: inputFormat)
+    player.pointee.engine.connect(mixerNode, to: outputNode, format: inputFormat)
     print ("Bottom of createMyAVEngine()")
 }
+
+#if PART_II
+func prepareSpeechAU(player: UnsafeMutablePointer<MyAUEnginePlayer>) throws {
+    let chan = try player.pointee.speechUnit.getSpeechChannel()
+    SpeakCFString(chan, Settings.speakString, nil)
+    print ("Bottom of prepareSpeechAU()")
+}
+#endif
 
 func main() throws {
     var player = MyAUEnginePlayer()
@@ -178,16 +224,20 @@ func main() throws {
     
     // build an engine with the output unit
     try createMyAVEngine(player: &player)
+#if PART_II
+    // Configure the speech synthesizer
+    try prepareSpeechAU(player: &player)
+#endif
     
-    
-    try player.inputUnit.start()
+    // try player.inputUnit.start()
     player.engine.prepare()
     try player.engine.start()
+    defer {
+        player.engine.stop()
+        player.engine.reset()
+    }
     
     // and wait
     print ("Capturing, press <return> to stop:")
     getchar()
-
-
-
 }
